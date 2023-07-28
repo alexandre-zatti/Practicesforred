cfff9d81eea6426249bb25a6024a9a48b1bdf844
Alexandre Zatti <alexandre.z@cresol.com.br>
2023-07-27 22:12:49 -0300

    Iniciado desenvolvimento backend

---
+++ backend/src/test/java/zatti/alexandre/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zatti.alexandre.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
